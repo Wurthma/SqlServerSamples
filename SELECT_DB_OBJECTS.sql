91888e33068f7257d8f419c9443e354f37550f91
George Henrique Wurthmann <gemakai@gmail.com>
2021-02-25 14:38:21 -0300

    Update SELECT_DB_OBJECTS.sql

--- SELECT_DB_OBJECTS.sql
+++ SELECT_DB_OBJECTS.sql
@@ -29,3 +29,30 @@ SELECT *
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_NAME = N'TABLE_EXAMPLE'
 AND COLUMN_NAME = N'THE_CLOUMN'
+
+-- ALL INDEXES
+-- https://stackoverflow.com/a/765892/5522115
+SELECT 
+     TableName = t.name,
+     IndexName = ind.name,
+     IndexId = ind.index_id,
+     ColumnId = ic.index_column_id,
+     ColumnName = col.name,
+     ind.*,
+     ic.*,
+     col.* 
+FROM 
+     sys.indexes ind 
+INNER JOIN 
+     sys.index_columns ic ON  ind.object_id = ic.object_id and ind.index_id = ic.index_id 
+INNER JOIN 
+     sys.columns col ON ic.object_id = col.object_id and ic.column_id = col.column_id 
+INNER JOIN 
+     sys.tables t ON ind.object_id = t.object_id 
+WHERE 
+     ind.is_primary_key = 0 
+     AND ind.is_unique = 0 
+     AND ind.is_unique_constraint = 0 
+     AND t.is_ms_shipped = 0 
+ORDER BY 
+     t.name, ind.name, ind.index_id, ic.is_included_column, ic.key_ordinal;
